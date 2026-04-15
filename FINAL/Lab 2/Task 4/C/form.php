cab7b49dc35adbcb8e9748e384d6ba2c07e9cb06
Abtahi Islam <143752829+Abtahi360@users.noreply.github.com>
2026-04-15 22:27:55 +0600

    Completed Task 5(A) for lab 2

--- FINAL/Lab 2/Task 4/C/form.php
+++ FINAL/Lab 2/Task 4/C/form.php
@@ -26,7 +26,6 @@ if(isset($_POST['submit'])){
     <input type="radio" name="gender" value="Male" <?php if(isset($gender) && $gender=="Male"){ echo "checked"; } ?>/> Male
     <input type="radio" name="gender" value="Female" <?php if(isset($gender) && $gender=="Female"){ echo "checked"; } ?>/> Female
     <input type="radio" name="gender" value="Other" <?php if(isset($gender) && $gender=="Other"){ echo "checked"; } ?>/> Other
-
     <br><br>
     <input type="submit" name="submit" value="Submit"/>
 </form>
