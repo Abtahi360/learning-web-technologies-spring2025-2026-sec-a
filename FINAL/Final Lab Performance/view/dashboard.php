92704293402dd0e9b34f6ff0793e7d16d508c315
Abtahi360 <abtahibinislam2018@gmail.com>
2026-05-06 16:10:23 +0600

    Add employer registration form and search functionality to dashboard

--- FINAL/Final Lab Performance/view/dashboard.php
+++ FINAL/Final Lab Performance/view/dashboard.php
@@ -1,6 +1,35 @@
+<?php
+session_start();
+require_once('../model/jobModel.php');
+if(!isset($_COOKIE['status'])){
+    header('location: login.php');
+}
+$employers = getAllEmployers();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Employer Dashboard</title>
 </head>
 <body>
+    <h1>Welcome Home <?php echo $_SESSION['username']; ?></h1>
+    <a href="../controller/action.php?action=logout">Logout</a>
+    <br><br>
+
+    <form method="post" action="../controller/action.php?action=create_employer" enctype="">
+        <fieldset>
+            <legend>Register New Employer</legend>
+            Employer Name: <input type="text" name="employer_name" value="" /> <br>
+            Company Name: <input type="text" name="company_name" value="" /> <br>
+            Contact No: <input type="text" name="contact_no" value="" /> <br>
+            Username: <input type="text" name="username" value="" /> <br>
+            Password: <input type="password" name="password" value="" /> <br>
+            <input type="submit" name="submit" value="Register" />
+        </fieldset>
+    </form>
+
+    <br>
+    Search Employer: <input type="text" id="search_key" onkeyup="searchEmployers()" />
+    <br><br>
